c3ae09a3e5a3b55677bc34b6a77e79158d74aaeb
Altoid0 <39836458+Altoid0@users.noreply.github.com>
2023-04-05 09:48:02 -0700

    Add files via upload

---
+++ Program2.asm
@@ -0,0 +1,8 @@
+#Name: Tony Diaz, Xavier Felix, Tanay Shah, Clifton Williams
+#Date 4/5/2023
+#Assignment: Program 2: Practice with Cnditionals and Loops
+#Objectives
+# Display main mento to user
+# take int input from user
+# Depening on input and ask for grade percentage
+# based on inputted value display the correct grade letter to the user
